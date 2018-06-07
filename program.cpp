3f379a81a384a2606ae814c2cefcd8ff63380ff0
935963004 <38138106+935963004@users.noreply.github.com>
2018-06-07 17:00:28 +0800

    Update program.cpp

--- program.cpp
+++ program.cpp
@@ -13,27 +13,27 @@ bool equal(const char *a, const char *b)
 }
 int main()
 {
+	freopen("5.in", "r", stdin);
 	backend tmp;
 	tmp.init();
 	char op[50];
 	while (true) {
-		scanf("%s", op);
+		//scanf("%s", op);
+		cin >> op;
+		char *s;
 		if (equal(op, "register"))
 			printf("%d\n", tmp.regist());
 		else if (equal(op, "login"))
 			printf("%d\n", tmp.login());
 		else if (equal(op, "query_profile")) {
-			int x;
-			scanf("%d", &x);
-			printf("%s\n", tmp.query_profile(x));
+			s = tmp.query_profile();
+			printf("%s\n", s);
+			delete[]s;
 		}
 		else if (equal(op, "modify_profile"))
 			printf("%d\n", tmp.modify_profile());
-		else if (equal(op, "modify_privilege")) {
-			int id1, id2, pri;
-            		scanf("%d%d%d", &id1, &id2, &pri);
-			printf("%d\n", tmp.modify_privilege(id1, id2, pri));
-		}
+		else if (equal(op, "modify_privilege"))
+			printf("%d\n", tmp.modify_privilege());
 		else if (equal(op, "add_train"))
 			printf("%d\n", tmp.add_train());
 		else if (equal(op, "modify_train"))
@@ -42,24 +42,34 @@ int main()
 			printf("%d\n", tmp.sale_train());
 		else if (equal(op, "delete_train"))
 			printf("%d\n", tmp.delete_train());
-		else if (equal(op, "query_train"))
-			printf("%s\n", tmp.query_train());
+		else if (equal(op, "query_train")) {
+			s = tmp.query_train();
+			printf("%s\n", s);
+			delete[]s;
+		}
 		else if (equal(op, "query_ticket")) {
-			char *s = tmp.query_ticket();
+			s = tmp.query_ticket();
 			//printf("%s\n", s);
 			int len = strlen(s);
 			for (int i = 0; i < len; ++i)
 				printf("%c", s[i]);
 			printf("\n");
+			delete[]s;
 		}
 		else if (equal(op, "buy_ticket"))
 			printf("%d\n", tmp.buy_ticket());
-		else if (equal(op, "query_order"))
-			printf("%s\n", tmp.query_order());
+		else if (equal(op, "query_order")) {
+			s = tmp.query_order();
+			printf("%s\n", s);
+			delete[]s;
+		}
 		else if (equal(op, "refund_ticket"))
 			printf("%d\n", tmp.refund_ticket());
-		else if (equal(op, "query_transfer"))
-			printf("%s\n", tmp.query_transfer());
+		else if (equal(op, "query_transfer")) {
+			s = tmp.query_transfer();
+			printf("%s\n", s);
+			delete[]s;
+		}
 		else if (equal(op, "clean")) {
 			tmp.clean();
 			printf("1\n");
